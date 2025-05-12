f81ec7aa8896bbeac82b36a97f0cb15f7463c7e7
michal1454 <mkrawczykowski@hotmail.com>
2025-05-12 23:54:32 +0200

    wip: add read functionality

---
+++ utils/model.py
@@ -0,0 +1,6 @@
+users:list=[
+    {'name':'Michał','location':'Iwaniska','posts':100},
+    {'name':'Maciej','location':'Piotrów','posts':100},
+    {'name':'Kurdziel','location':'Obręczno','posts':100},
+    {'name':'Konrad','location':'Wypiździszowo','posts':100},
+]
