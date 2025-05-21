dfcc314c00f109f55d5279fc7dc43247ff65dc57
michal1454 <mkrawczykowski@hotmail.com>
2025-05-21 16:15:19 +0200

    feat: mpa GUI fully working

---
+++ notatnik.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self):
+        self.name='Tomek'
+        self.surname='Tomek'
+        self.location='Tomek'
+        self.posts= post
+
+user_01=User(name='John')
+user_02=User(name='Johny')
+
+print(user_01.name)
+print(user_02.name)
